ad53e3d3c52188a0efb60ed11fcda98d79dc99ab
Chris <hi@cblakely.dev>
2019-10-31 15:46:57 +0000

    adding names to form fields

--- src/components/common/ContactForm.js
+++ src/components/common/ContactForm.js
@@ -4,16 +4,16 @@ const ContactForm = () => (
 	<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
 		<input type="hidden" name="form-name" value="contact" />
 		<div className="wrap">
-			<input id="name" type="text" placeholder="Your Name*" required />
+			<input id="name" type="text" placeholder="Your Name*" name="name" required />
 		</div>
 		<div className="wrap">
-			<input id="email" type="text" placeholder="Your Email*" required />
+			<input id="email" type="text" placeholder="Your Email*" name="email" equired />
 		</div>
 		<div className="wrap">
-			<input id="phone" type="text" placeholder="Your Phone Number*" required />
+			<input id="phone" type="text" placeholder="Your Phone Number*" name="phone" required />
 		</div>
 		<div className="wrap">
-			<textarea id="message" placeholder="Your Message*" required></textarea>
+			<textarea id="message" placeholder="Your Message*" name="message" required></textarea>
 		</div>
 		<div className="arlo_tm_button">
 			<button type="submit"><span>Send Message</span></button>
